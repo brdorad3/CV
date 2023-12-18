cce16d9653830a0a42a794e50d75f78aeb5307e2
adis <vunicadis@gmail.com>
2023-12-18 17:05:36 +0100

    Sidebar

---
+++ CVapp/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Sidebar from './sidebar'
+
+import './App.css'
+
+function App() {
+
+
+ 
+  return (
+    <>
+    <div className="app">
+    <Sidebar></Sidebar>
+  </div>
+    </>
+  )
+}
+
+export default App
